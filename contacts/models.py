43d3a0839704baf33a27b6680a5d34ab30aa9291
rolycore <rolycore@gmail.com>
2024-07-19 12:56:22 -0500

    Creando Repositorio backend contact-app

---
+++ contacts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
---
+++ contacts/urls.py
@@ -0,0 +1,12 @@
+# urls.py en tu aplicación Django
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ContactViewSet
+
+router = DefaultRouter()
+router.register(r'contacts', ContactViewSet, basename='contact')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
